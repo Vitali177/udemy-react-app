f67f8dd6dd2a140db18e107dbc58b5407269cd11
Vitali <vskrabatun@gmail.com>
2020-06-20 18:56:51 +0300

    feat: add states and props for components

--- src/components/post-status-filter/post-status-filter.js
+++ src/components/post-status-filter/post-status-filter.js
@@ -5,8 +5,8 @@ import './post-status-filter.css';
 const PostStatusFilter = () => {
   return (
     <div className="btn-group">
-      <button/>
-      <button/>
+      <button type="button" className="btn btn-info">Всё</button>
+      <button type="button" className="btn btn-outline-secondary">Понравилось</button>
     </div>
   )
 }
